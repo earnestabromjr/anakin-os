e1b060cd712f3afde1244ac3e1c53425b9a35bf5
earnestabromjr <abromearnest@gmail.com>
2026-05-13 16:17:46 -0500

    Added install packages

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,7 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 install -y tmux ripgrep fd-find fzf zoxide bat lsd virt-manager gcc rust cargo unzip
 
 # Use a COPR Example:
 #
